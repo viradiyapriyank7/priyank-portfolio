6ae280a9e8d41b3a35182ecd06ec741b7735a5bf
priyank viradiya <priyankviradiya227@gmail.com>
2025-10-05 18:00:47 +0530

    issue solve

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -110,7 +110,7 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 transform -translate-x-1/2 animate-bounce">
         <ArrowDown size={32} className="text-primary" />
       </div>
     </section>
